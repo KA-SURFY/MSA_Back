ee05e85ce93e99b59765840a3d030d9084d6c5d4
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 22:33:22 +0900

    Update WriteQuestionRepository.java
    edit SurveyService - trying to solve created-survey

--- survey/src/main/java/surfy/comfy/repository/write/WriteQuestionRepository.java
+++ survey/src/main/java/surfy/comfy/repository/write/WriteQuestionRepository.java
@@ -1,10 +1,12 @@
 package surfy.comfy.repository.write;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.write.Question;
 
 import java.util.List;
 
+@Repository
 public interface WriteQuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findAllBySurveyId(Long surveyId);
 }
