16963b275c9f386bc3971d888daedbc80e497bd2
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 14:45:44 +0900

    Update ReadTokenRepository.java
    edit AuthService - test1; ReadtokenRepository

--- auth/src/main/java/surfy/comfy/repository/read/ReadTokenRepository.java
+++ auth/src/main/java/surfy/comfy/repository/read/ReadTokenRepository.java
@@ -1,11 +1,13 @@
 package surfy.comfy.repository.read;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.Token;
 
 import java.math.BigInteger;
 import java.util.Optional;
 
+@Repository
 public interface ReadTokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByRefreshToken(String refreshToken);
