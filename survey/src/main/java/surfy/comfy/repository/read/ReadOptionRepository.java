b7fec528fac22471c06bd724da90ecea075b4b11
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 22:33:35 +0900

    Update ReadOptionRepository.java
    edit SurveyService - trying to solve created-survey

--- survey/src/main/java/surfy/comfy/repository/read/ReadOptionRepository.java
+++ survey/src/main/java/surfy/comfy/repository/read/ReadOptionRepository.java
@@ -2,10 +2,12 @@ package surfy.comfy.repository.read;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.write.Option;
 
 import java.util.List;
 
+@Repository
 public interface ReadOptionRepository extends JpaRepository<Option, Long> {
     List<Option> findAllByQuestionId(Long QuestionId);
     List<Option> findAllBySurveyId(Long surveyId);
