d9c90600707eab0f5aa7ec1d5e9d1256efaadfcf
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 22:33:39 +0900

    Update ReadGridRepository.java
    edit SurveyService - trying to solve created-survey

--- survey/src/main/java/surfy/comfy/repository/read/ReadGridRepository.java
+++ survey/src/main/java/surfy/comfy/repository/read/ReadGridRepository.java
@@ -2,10 +2,12 @@ package surfy.comfy.repository.read;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.write.Grid;
 
 import java.util.List;
 
+@Repository
 public interface ReadGridRepository extends JpaRepository<Grid, Long> {
     List<Grid> findAllByQuestionId(Long QuestionId);
     List<Grid> findAllBySurveyId(Long surveyId);
