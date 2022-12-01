b10d35b6b04095d6d72ab31808e11ac29b6d23c2
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 17:14:23 +0900

    Update BookmarkService.java
    edit PostService - add author name

--- post/src/main/java/surfy/comfy/service/BookmarkService.java
+++ post/src/main/java/surfy/comfy/service/BookmarkService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import surfy.comfy.data.post.GetPostResponse;
+import surfy.comfy.entity.read.Member;
 import surfy.comfy.entity.write.Bookmark;
 import surfy.comfy.repository.read.ReadBookmarkRepository;
+import surfy.comfy.repository.read.ReadMemberRepository;
 import surfy.comfy.repository.read.ReadPostRepository;
 import surfy.comfy.repository.write.WriteBookmarkRepository;
 
@@ -19,6 +21,7 @@ public class BookmarkService {
     private final WriteBookmarkRepository writeBookmarkRepository;
     private final ReadBookmarkRepository readBookmarkRepository;
     private final ReadPostRepository readPostRepository;
+    private final ReadMemberRepository readMemberRepository;
 
     @Transactional
     public List<GetPostResponse> getBookmarks(Long memberId){
@@ -26,7 +29,8 @@ public class BookmarkService {
         List<GetPostResponse> bookmarks=new ArrayList<>();
 
         for(Bookmark b:bookmarkList){
-            GetPostResponse post=new GetPostResponse(b.getPost(),true,true);
+            Member member=readMemberRepository.findById(b.getPost().getMemberId()).get();
+            GetPostResponse post=new GetPostResponse(b.getPost(),member,true,true);
             bookmarks.add(post);
         }
         return bookmarks;
