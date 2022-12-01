ae8c4ae4db93681ccf99095e440551f843e938c3
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 22:33:28 +0900

    Update WriteGridRepository.java
    edit SurveyService - trying to solve created-survey

--- survey/src/main/java/surfy/comfy/repository/write/WriteGridRepository.java
+++ survey/src/main/java/surfy/comfy/repository/write/WriteGridRepository.java
@@ -2,10 +2,12 @@ package surfy.comfy.repository.write;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.write.Grid;
 
 import java.util.List;
 
+@Repository
 public interface WriteGridRepository extends JpaRepository<Grid, Long> {
     List<Grid> findAllByQuestionId(Long QuestionId);
     List<Grid> findAllBySurveyId(Long surveyId);
