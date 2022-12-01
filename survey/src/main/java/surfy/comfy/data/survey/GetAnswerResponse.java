27b467d9cc9b172419d8acda9d80a1da5f25a3c6
Lab00700 <zerglisk123@naver.com>
2022-12-01 14:20:50 +0900

    개별보기 fix fix fix ver1

--- survey/src/main/java/surfy/comfy/data/survey/GetAnswerResponse.java
+++ survey/src/main/java/surfy/comfy/data/survey/GetAnswerResponse.java
@@ -10,11 +10,14 @@ import java.util.List;
 @Data
 public class GetAnswerResponse {
     private List<Answer> answers;
-
-    @Autowired
     private ReadAnswerRepository readAnswerRepository;
 
+    @Autowired
+    public void callreadrepo(ReadAnswerRepository readAnswerRepository) {
+        this.readAnswerRepository=readAnswerRepository;
+    }
     public GetAnswerResponse(Long questionId){
         this.answers=readAnswerRepository.findAllByQuestionId(questionId);
     }
+
 }
