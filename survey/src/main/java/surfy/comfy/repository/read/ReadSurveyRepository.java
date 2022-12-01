90a26308c51bbfed03703356e751e71611a7e9ee
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 22:33:31 +0900

    Update ReadSurveyRepository.java
    edit SurveyService - trying to solve created-survey

--- survey/src/main/java/surfy/comfy/repository/read/ReadSurveyRepository.java
+++ survey/src/main/java/surfy/comfy/repository/read/ReadSurveyRepository.java
@@ -1,12 +1,14 @@
 package surfy.comfy.repository.read;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.write.Survey;
 import surfy.comfy.type.SurveyType;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReadSurveyRepository extends JpaRepository<Survey,Long> {
     Optional<Survey> findById(Long surveyId);
     Survey findSurveysById(Long surveyId);
