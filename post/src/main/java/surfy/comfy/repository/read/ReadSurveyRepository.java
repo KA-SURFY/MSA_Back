b699662d259189a833438c94568b3fa5d1c53981
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 13:13:21 +0900

    edit ReadSurveyRepository

--- post/src/main/java/surfy/comfy/repository/read/ReadSurveyRepository.java
+++ post/src/main/java/surfy/comfy/repository/read/ReadSurveyRepository.java
@@ -1,6 +1,7 @@
 package surfy.comfy.repository.read;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.read.Survey;
 import surfy.comfy.type.SurveyType;
 
@@ -8,6 +9,7 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReadSurveyRepository extends JpaRepository<Survey,Long> {
     Optional<Survey> findById(Long surveyId);
     Survey findSurveysById(Long surveyId);
