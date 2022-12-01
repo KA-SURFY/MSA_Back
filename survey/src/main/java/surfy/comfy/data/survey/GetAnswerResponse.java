eeabacd34f8b695b0986017fe9fe2754c0177a8b
Lab00700 <zerglisk123@naver.com>
2022-12-01 15:43:19 +0900

    개별보기 fix fix fix ver3

--- survey/src/main/java/surfy/comfy/data/survey/GetAnswerResponse.java
+++ survey/src/main/java/surfy/comfy/data/survey/GetAnswerResponse.java
@@ -2,6 +2,7 @@ package surfy.comfy.data.survey;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import surfy.comfy.entity.read.Answer;
 import surfy.comfy.repository.read.ReadAnswerRepository;
 
@@ -10,14 +11,16 @@ import java.util.List;
 @Data
 public class GetAnswerResponse {
     private List<Answer> answers;
-    private final ReadAnswerRepository readAnswerRepository = getReadAnswerRepository();
-
-    @Autowired
-    public ReadAnswerRepository getReadAnswerRepository() {
-        return readAnswerRepository;
-    }
+    private ReadAnswerRepository readAnswerRepository=new GetRepo().getReadAnswerRepository();
 
     public GetAnswerResponse(Long questionId){
         this.answers=readAnswerRepository.findAllByQuestionId(questionId);
     }
+}
+@Service
+@Data
+class GetRepo{
+    @Autowired
+    private ReadAnswerRepository readAnswerRepository;
+    public GetRepo(){}
 }
