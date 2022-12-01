6557a334079c41bb1921338c1a264491770530c2
minseo300 <lmslmsms0616@gmail.com>
2022-12-01 14:45:26 +0900

    Update WriteTokenRepository.java
    edit AuthService - test1; WriteTokenRepository

--- auth/src/main/java/surfy/comfy/repository/write/WriteTokenRepository.java
+++ auth/src/main/java/surfy/comfy/repository/write/WriteTokenRepository.java
@@ -1,11 +1,13 @@
 package surfy.comfy.repository.write;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import surfy.comfy.entity.Token;
 
 import java.math.BigInteger;
 import java.util.Optional;
 
+@Repository
 public interface WriteTokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByRefreshToken(String refreshToken);
