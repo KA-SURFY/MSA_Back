cb4c69f5be50093e304a540162eaebb3eb830787
minseo300 <lmslmsms0616@gmail.com>
2022-12-03 16:00:22 +0900

    Update Token.java
    remove foreign key - member

--- user/src/main/java/surfy/comfy/entity/Token.java
+++ user/src/main/java/surfy/comfy/entity/Token.java
@@ -20,8 +20,7 @@ public class Token {
     private String refreshToken;
     private String refreshTokenIdxEncrypted;
 
-    @OneToOne
-    @JsonBackReference
-    @JoinColumn(name="member_id")
-    private Member member;
+
+    @Column(name="member_id")
+    private Long memberId;
 }
